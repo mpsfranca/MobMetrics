df771972a77196a7c3b2b84b06d09d967f3f0e57
Márcio <marciopc.filho27@gmail.com>
2025-04-30 10:57:00 -0300

    fix: GlobalMetrics bug

--- MobMetrics/metrics/urls.py
+++ MobMetrics/metrics/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload/', views.upload_view, name='upload'),
+    path('', views.upload_view, name='upload'),
     path('delete/', views.delete_view, name='delete'),
     path('download/', views.download_zip_view, name='download'),
     path('DataAnalytics/', views.data_analytics_view, name='DataAnalytics'),
