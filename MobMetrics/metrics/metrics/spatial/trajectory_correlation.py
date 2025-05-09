974fd4edc2398334bebebc08bfe55cf8b2de7d3d
Lucas Novais <lucasnovais222@gmail.com>
2025-05-09 10:47:38 -0300

    Implementing the intiial version of the trajectory correlation index

---
+++ MobMetrics/metrics/metrics/spatial/trajectory_correlation.py
@@ -0,0 +1,40 @@
+from ..utils.abs_metric import AbsMetric
+from sklearn.metrics.pairwise import cosine_similarity
+
+import numpy as np
+
+class TrajectoryCosineStd(AbsMetric):
+    def __init__(self, trace):
+        self.trace = trace
+        self.num_points = 20
+
+    def _uniform_sample_traj(self, df_id):
+        df_id_sorted = df_id.sort_values('time')
+
+        if len(df_id_sorted) < self.num_points:
+            return None
+        
+        sampled = df_id_sorted.iloc[np.linspace(0, len(df_id_sorted) - 1, self.num_points, dtype=int)]
+        return sampled[['x', 'y']].to_numpy().flatten()
+
+    def extract(self):
+        vectors = []
+        for ent_id, df_ent in self.trace.groupby('id'):
+            traj_vector = self._uniform_sample_traj(df_ent)
+
+            if traj_vector is not None:
+                vectors.append(traj_vector)
+
+        if len(vectors) < 2:
+            return 0.0
+
+        vectors = np.array(vectors)
+        sim_matrix = cosine_similarity(vectors)
+
+        upper_indices = np.triu_indices_from(sim_matrix, k=1)
+        sim_values = sim_matrix[upper_indices]
+
+        dist_values = 1 - sim_values
+        std_dev = np.std(dist_values)
+
+        return round(std_dev, 5)
