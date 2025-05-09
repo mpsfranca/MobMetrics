684a5ba2ec390542b67abddde4254c01512172f2
Lucas Novais <lucasnovais222@gmail.com>
2025-05-09 13:26:01 -0300

    Fixing points sampling model and adding docs

--- MobMetrics/metrics/metrics/spatial/trajectory_correlation.py
+++ MobMetrics/metrics/metrics/spatial/trajectory_correlation.py
@@ -7,17 +7,37 @@ import tqdm
 
 class TrajectoryCorrelationDegree(AbsMetric):
     def __init__(self, trace, parameters):
+        """
+        Class that represents the Trajectory Correlation Degree metric. The more similar the trajectories
+        of the entities present in the trace are, the higher the index represented by this measure will be.
+
+        Attributes:
+            `trace` (DataFrame): trace data with coordinates, timestamp and identifier
+            `parameters` (list): configuration parameters for processing
+            `fraction` (float): fraction of points at which trajectories will be reduced
+            `min_points` (int): minimal number of points at which trajectories will be reduced
+        """
         self.trace = trace
         self.parameters = parameters
-        self.num_points = 20
+
+        self.fraction = 0.5
+        self.min_points = 20
 
     def _uniform_sample_traj(self, df_id):
         df_id_sorted = df_id.sort_values('time')
 
-        if len(df_id_sorted) < self.num_points:
+        n_points_id = max(
+            self.min_points,
+            int(self.fraction * len(df_id_sorted))
+        )
+
+        if len(df_id_sorted) < n_points_id:
             return None
         
-        sampled = df_id_sorted.iloc[np.linspace(0, len(df_id_sorted) - 1, self.num_points, dtype=int)]
+        sampled = df_id_sorted.iloc[
+            np.linspace(0, len(df_id_sorted) - 1, n_points_id, dtype=int)
+        ]
+        
         return sampled[['x', 'y']].to_numpy().flatten()
 
     def extract(self):
